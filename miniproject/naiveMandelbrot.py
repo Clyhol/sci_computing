a4abdf27501c6523f7e71d452777ff0404b66eab
Christoffer <clyhol20@student.aau.dk>
2025-02-26 21:22:35 +0100

    numba and timing implemented, and comment clean up

--- miniproject/naiveMandelbrot.py
+++ miniproject/naiveMandelbrot.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
+from numba import jit
+from timeit import default_timer as timer
 
+# fastmath uses less accurate but faster operations
+@jit(nopython = True, fastmath = True)
 def compute_mandelbrot(c, max_iters, threshold):
     z = 0
     for n in range(max_iters):
@@ -12,30 +16,34 @@ def compute_mandelbrot(c, max_iters, threshold):
             z = z**2 + c
     return  max_iters # return max_iters if the point doesn't diverge after max_iters iterations
 
+@jit(nopython = True, fastmath = True)
 def mandelbrot_set(re_min, re_max, im_min, im_max, p_re, p_im, max_iters, threshold):
-    re = np.linspace(re_min, re_max, p_re) # real part
-    im = np.linspace(im_min, im_max, p_im) # imaginary part
+    re = np.linspace(re_min, re_max, p_re)
+    im = np.linspace(im_min, im_max, p_im)
     
-    mset = np.zeros((p_re, p_im)) # create an array to store the mandelbrot set
+    mset = np.zeros((p_im, p_re)) # create an array to store the mandelbrot set
     
-    for i in tqdm(range(p_im), desc="Calculating Mandelbrot set"): # rows (y-axis)
+    for i in range(p_im): # rows (y-axis)
         for j in range(p_re): # columns (x-axis)
             c = re[j] +1j * im[i] # create a starting point
-            mset[i, j] = compute_mandelbrot(c, max_iters, threshold) # store the number of iterations to diverge
+            mset[i, j] = compute_mandelbrot(c, max_iters, threshold) # store the number of iterations before diverge
     return mset
 
 
 if __name__ == "__main__":
-    p_re = 5000
-    p_im = 5000
+    p_re = 5000 # number of points in the real part
+    p_im = 5000 # number of points in the imaginary part
     re_min, re_max = -2.0, 1.0
     im_min, im_max = -1.5, 1.5
-    max_iters = 100 # 1000 iters, 14:32 runtime. 100 iters, 2:32 runtime
+    max_iters = 100
     threshold = 2
     outputdir = "miniproject/output"
-    os.mkdir(outputdir, exist_ok=True)
+    os.makedirs(outputdir, exist_ok=True)
     
+    print("Computing Mandelbrot set...")
+    start_time = timer()
     mset = mandelbrot_set(re_min, re_max, im_min, im_max, p_re, p_im, max_iters, threshold)
+    print(f"Execution time: {timer() - start_time}s")
     
     plt.imshow(mset, extent=[re_min, re_max, im_min, im_max])
     plt.set_cmap('hot')
@@ -43,6 +51,9 @@ if __name__ == "__main__":
     plt.show()
     plt.imsave(os.path.join(outputdir, f"{os.path.basename(__file__).split(".")[0]}_{max_iters}_iters.pdf"), mset)
     
+    # runtime for 100 iterations
+    # JIT:      8.27s
+    # No JIT:   203.71s 
     
             
     
--- miniproject/vectorMandelbrot.py
+++ miniproject/vectorMandelbrot.py
@@ -2,16 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
+from numba import jit
+from timeit import default_timer as timer
 
 def vector_mandelbrot_set(re_min, re_max, im_min, im_max, p_re, p_im, max_iters, threshold):
-    re = np.linspace(re_min, re_max, p_re) # real part
-    im = np.linspace(im_min, im_max, p_im) # imaginary part
+    re = np.linspace(re_min, re_max, p_re)
+    im = np.linspace(im_min, im_max, p_im)
     C = re[np.newaxis, :] + 1j * im[:, np.newaxis] # create axis across columns for real part and rows for imaginary part
     
-    z = np.zeros_like(C)
-    mset = np.zeros(C.shape, dtype=int) # array to store number of iterations before divergence
+    z = np.copy(C) # populate z with starting points
+    mset = np.zeros(C.shape) # array to store number of iterations before divergence
     
-    for _ in tqdm(range(max_iters), desc="Calculating Mandelbrot set"):
+    for _ in tqdm(range(max_iters), desc="Computing Mandelbrot set"):
         mask = np.abs(z) <= threshold
         z[mask] = z[mask]**2 + C[mask] # perform operation on points that have not diverged
         mset += mask # add +1 to all points with true in the mask
@@ -22,7 +24,7 @@ if __name__ == "__main__":
     p_im = 5000
     re_min, re_max = -2.0, 1.0
     im_min, im_max = -1.5, 1.5
-    max_iters = 100 # 100 iterations runtime: 19s
+    max_iters = 100
     threshold = 2
     outputdir = "miniproject/output"
     os.makedirs(outputdir, exist_ok=True)
@@ -35,6 +37,8 @@ if __name__ == "__main__":
     plt.show()
     plt.imsave(os.path.join(outputdir, f"{os.path.basename(__file__).split(".")[0]}_{max_iters}_iters.pdf"), mset)
     
+    # runtime for 100 iterations
+    # No JIT:   18s
     
             
     
